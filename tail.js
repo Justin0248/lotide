2c3b0708672091e29056c0eb4e29c0a76c940336
Yonin408 <justin12689@gmail.com>
2023-03-13 15:26:17 -0700

    implemented the assertEqual function using require()

--- tail.js
+++ tail.js
@@ -1,14 +1,6 @@
 const tail = function(arr) {
-    return console.log(arr[arr.length - 1]);
+    return (arr[arr.length - 1]);
 }
-
-
-const assertEqual = function(actual, expected) {
-    if (actual !== expected) {
-      return console.log(`Assertion Failed ${actual} != ${expected}`);
-    }
-    return console.log(`Assertion Passed ${actual} === ${expected}`);
-   
-  };
-  const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
+const assertEqual = require('./assertEqual');
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+assertEqual(tail(words), 'Labs');
